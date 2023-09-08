dde819e82c0e31554618e4543807b45dae03d122
pearl-aqua <pearlaquagreenblood@gmail.com>
2023-09-08 21:51:18 +0900

    빌드 에러 수정

--- src/app/complete/page.tsx
+++ src/app/complete/page.tsx
@@ -1,14 +1,11 @@
 import Complete from "@/components/complete/Complete";
 import ResultContainer from "@/components/complete/ResultContainer";
-import { getResult } from "@/firebase/apply";
 
 export default async function CompletePage() {
-  const data = await getResult();
-
   return (
     <div className="flex flex-col justify-center items-center gap-5">
       <Complete />
-      <ResultContainer data={data} />
+      <ResultContainer />
     </div>
   );
 }
