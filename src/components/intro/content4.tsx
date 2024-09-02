2740a6544c35af79c56e27144e4367426bc2e080
pearl-aqua <pearlaquagreenblood@gmail.com>
2024-09-02 18:35:38 +0900

    안쓰는 파일 삭제

--- src/components/intro/content4.tsx
+++
@@ -1,9 +0,0 @@
-import ResultContainer from "../poll/ResultContainer";
-
-export default function Home() {
-  return (
-    <div className="flex flex-col justify-center items-center gap-4">
-      <ResultContainer />
-    </div>
-  );
-}
